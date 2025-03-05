6e1b365ba625f5782ba6629857550251d90447d4
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-03-05 16:44:42 -0300

    observacoes

--- routes/agendamento.js
+++ routes/agendamento.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const agendamentoController = require('../controller/agendamentoController.js')
 const WithAuth = require('../middleware/auth.js');
 
+// Falta deletar o agendamento
 router.post('/agendamentos', agendamentoController.createAgendamento);
 router.get('/agendamentos', WithAuth, agendamentoController.getAllAgendamentos);
 
--- routes/servico.js
+++ routes/servico.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const servicoController = require('../controller/servicoController.js');
 const WithAuth = require('../middleware/auth.js');
 
+// falta buscar, deletar e atualizar o cliente
+
 router.post('/servico', servicoController.createServico);
+// Essa requisicao nao faz sentido
 router.post('/servico', (req, res) => {
     
     res.send("Serviço cadastrado!");
