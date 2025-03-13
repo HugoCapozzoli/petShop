48107c207ee3011e6bd2d89a534e07f6a63f66da
Pedro Henrique <138126897+Mechph@users.noreply.github.com>
2025-03-13 10:05:23 -0300

    Adicionando mensagens de retorno
    Foi adicionado mensagens de texto, para deixar o sistema uma identidade um pouco mais profissional.

--- controller/servicoController.js
+++ controller/servicoController.js
@@ -6,17 +6,24 @@ const createServico = async (req, res) => {
         const { nome, valor, duracaoMaxMinutos } = req.body;
         const newServico = new servicoModel({ nome, valor, duracaoMaxMinutos });
         await newServico.save();
-        res.status(201).json(newServico);
+        res.status(201).json({ 
+            message: "Serviço criado com sucesso!!",
+            servico: newServico 
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
 
+
 // Pega todos os ID
 const getAllServicos = async (req, res) => {
     try {
         const servicos = await servicoModel.find();
-        res.status(200).json(servicos);
+        res.status(200).json({
+            message: "Aqui estão todos os serviços cadastrados",
+            servicos
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -26,28 +33,43 @@ const getAllServicos = async (req, res) => {
 const getServicoById = async (req, res) => {
     try {
         const servico = await servicoModel.findById(req.params.id);
-        if (!servico) return res.status(404).json({ message: 'Serviço não encontrado' });
-        res.status(200).json(servico);
+        if (!servico) {
+            return res.status(404).json({ message: 'Serviço não encontrado' });
+        }
+
+        res.status(200).json({ 
+            message: "Serviço encontrado!!",
+            servico 
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
 
+
 // att por ID
 const updateServico = async (req, res) => {
     try {
-        const { name, valor, duracaoMaxMinutos } = req.body;
+        const { nome, valor, duracaoMaxMinutos } = req.body;
         const servico = await servicoModel.findByIdAndUpdate(
             req.params.id,
             { nome, valor, duracaoMaxMinutos },
-            { new: true } //* doc atualizado retorno
+            { new: true } //* Retorna o documento atualizado
         );
-        if (!servico) return res.status(404).json({ message: 'Serviço não encontrado' });
-        res.status(200).json(servico);
+        
+        if (!servico) {
+            return res.status(404).json({ message: 'Serviço não encontrado' });
+        }
+
+        res.status(200).json({ 
+            message: "Serviço atualizado com sucesso!!",
+            servico 
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
+
 
 //*  excluir serviço
 const deleteServico = async (req, res) => {
